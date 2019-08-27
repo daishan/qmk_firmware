63bacabcbe2a8f9a444ad1f48f573085b4a2cd98
Andreas Hemel <dai.shan@gmx.net>
2019-08-27 14:46:46 +0200

    */daishan: align layouts visually

--- keyboards/kbdfans/kbd8x/keymaps/daishan/keymap.c
+++ keyboards/kbdfans/kbd8x/keymaps/daishan/keymap.c
@@ -18,7 +18,6 @@
 #include "daishan.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     [BASE] = LAYOUT_tkl_ansi(
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,      KC_PSCR, KC_SLCK, KC_PAUS,
         KC_GRV,  ___1___, ___2___, ___3___, ___4___, ___5___, ___6___, ___7___, ___8___, ___9___, ___0___, KC_MINS, KC_EQL,    KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,
