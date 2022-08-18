bcc889ee608f5e0af98f604b5866fe43e2f93fb4
Andreas Hemel <andreas.hemel@namics.com>
2022-08-18 16:28:16 +0200

    users/daishan: replace removed send_unicode_hex_string() and handrolled stuff with send_unicode_string()

--- users/daishan/daishan.c
+++ users/daishan/daishan.c
@@ -18,45 +18,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch(keycode) {
             #ifdef UNICODE_ENABLE
             case SHRUG:
-                unicode_input_start(); register_hex(0xaf); unicode_input_finish();
-                TAP_ONCE(KC_BSLS);
-                register_code(KC_RSFT); TAP_ONCE(KC_MINS); TAP_ONCE (KC_9); unregister_code(KC_RSFT);
-                unicode_input_start(); register_hex(0x30c4); unicode_input_finish();
-                register_code(KC_RSFT); TAP_ONCE(KC_0); TAP_ONCE(KC_MINS); unregister_code(KC_RSFT);
-                TAP_ONCE(KC_SLSH);
-                unicode_input_start(); register_hex(0xaf); unicode_input_finish();
+                send_unicode_string("¯\\_ツ_/¯");
                 return false;
 
             case POOP:
-                send_unicode_hex_string("1f4a9");
+                send_unicode_string("💩");
                 return false;
 
             case FLIPTAB:
-                register_code(KC_RSFT);
-                TAP_ONCE(KC_9);
-                unregister_code(KC_RSFT);
-                process_unicode((0x256F | QK_UNICODE), record); // Arm
-                process_unicode((0x00B0 | QK_UNICODE), record); // Eye
-                process_unicode((0x25A1 | QK_UNICODE), record); // Mouth
-                process_unicode((0x00B0 | QK_UNICODE), record); // Eye
-                register_code(KC_RSFT);
-                TAP_ONCE(KC_0);
-                unregister_code(KC_RSFT);
-                process_unicode((0x256F | QK_UNICODE), record); // Arm
-                TAP_ONCE(KC_SPC);
-                process_unicode((0x0361 | QK_UNICODE), record); // Flippy
-                TAP_ONCE(KC_SPC);
-                process_unicode((0x253B | QK_UNICODE), record); // Table
-                process_unicode((0x2501 | QK_UNICODE), record); // Table
-                process_unicode((0x253B | QK_UNICODE), record); // Table
+                send_unicode_string("(╯°□°╯) ͡ ┻━┻");
                 return false;
 
             case FLIPTA2:
-                send_unicode_hex_string("0028 30CE 0CA0 75CA 0CA0 0029 30CE 5F61 253B 2501 253B");
+                send_unicode_string("(ノಠ痊ಠ)ノ彡┻━┻");
                 return false;
 
             case THUMBUP:
-                send_unicode_hex_string("1F44D");
+                send_unicode_string("👍");
                 return false;
             #endif
         }
